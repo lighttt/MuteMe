69bed64be960e67807785eaf94e0cb6a20a5fb68
Manish Tuladhar <manish.eclipse@gmail.com>
2020-08-27 13:32:11 +0545

    Adding google api client and places for picker

--- app/src/main/java/np/com/manishtuladhar/muteme/MainActivity.java
+++ app/src/main/java/np/com/manishtuladhar/muteme/MainActivity.java
@@ -1,18 +1,44 @@
 package np.com.manishtuladhar.muteme;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
+import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlacePicker;
+
+
+import np.com.manishtuladhar.muteme.provider.PlaceContract;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private static final String TAG = "MainActivity";
+
 
     //values
     private RecyclerView mRecyclerView;
 
+    //permission
+    private static final int PERMISSION_REQUEST_LOCATION = 111;
+    private static final int PLACE_REQ_CODE = 1;
 
 
     @Override
@@ -24,5 +50,99 @@ public class MainActivity extends AppCompatActivity {
         mRecyclerView = findViewById(R.id.places_rv);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        GoogleApiClient client = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .addApi(Places.GEO_DATA_API)
+                .enableAutoManage(this,this)
+                .build();
+    }
+
+    // ================== PLACE PICKER ===========================
+
+    /**
+     * Adding new location and ask permission with user
+     */
+    public void addNewLocation(View view) {
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+        != PackageManager.PERMISSION_GRANTED)
+        {
+            Toast.makeText(this, "Please grant us your location", Toast.LENGTH_SHORT).show();
+            return;
+        }
+       // Toast.makeText(this, "Location granted. Thank you!", Toast.LENGTH_SHORT).show();
+        try{
+            PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+            Intent i = builder.build(this);
+            startActivityForResult(i,PLACE_REQ_CODE);
+        }
+        catch (Exception e)
+        {
+            Log.e(TAG, "addNewLocation: Exception " +e.getMessage() );
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PLACE_REQ_CODE && resultCode == RESULT_OK) {
+            Place place = PlacePicker.getPlace(this, data);
+            if (place == null) {
+                Log.e(TAG, "onActivityResult: No place selected");
+                return;
+            }
+            String placeName = place.getName().toString();
+            String placeAddress = place.getAddress().toString();
+            String placeId = place.getId();
+
+            // insert into db
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(PlaceContract.PlaceEntry.COLUMN_PLACE_ID, placeId);
+            getContentResolver().insert(PlaceContract.PlaceEntry.CONTENT_URI, contentValues);
+        }
+    }
+
+    // ================== LOCATION PERMISSIONS ===========================
+
+    /**
+     * When user enables the location
+     */
+    public void onLocationEnabled(View view) {
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+               PERMISSION_REQUEST_LOCATION );
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SwitchCompat locationPermission = findViewById(R.id.enable_location);
+        if(ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            locationPermission.setChecked(false);
+        }
+        else{
+            locationPermission.setChecked(true);
+            locationPermission.setEnabled(false);
+        }
+    }
+
+    // ================== GOOGLE API CLIENT ===========================
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.e(TAG, "onConnected: connection successful");
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.e(TAG, "onConnectionSuspended: connection Suspended");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.e(TAG, "onConnectionFailed: connection Failed");
     }
 }
